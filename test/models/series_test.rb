56065d21b2581fcecb041241f0349ac3923a0f44
madtypist <mad_typist@yahoo.com>
2014-06-07 23:45:07 -0400

    Add a failing test to check that title is filled out

--- test/models/series_test.rb
+++ test/models/series_test.rb
@@ -4,4 +4,8 @@ class SeriesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should have a title" do
+    series = Series.create
+    assert_not series.save, "Saved the series without a title"
+  end
 end
