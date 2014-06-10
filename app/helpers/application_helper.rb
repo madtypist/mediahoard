28182ff085886ff0819c47a94d98c5950151a8fe
madtypist <mad_typist@yahoo.com>
2014-06-09 22:54:04 -0400

    Clean up some styles. Change function name to something that better explains what the helper function is meant to do.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
-  def needButtons(glyphiconType, btnText)
-      # do you work?
-      return '<span class="glyphicon-class glyphicon '+ glyphiconType + ' white"></span> <span class="white">'+ btnText +'</span>'
+  def styleButtons(glyphiconType, btnText)
+    # Keep the code clean by placing the button styling stuff here
+    # Need to do this because the default glyphicon fonts don't look right with the Bootstrap buttons
+    return '<span class="glyphicon-class glyphicon '+ glyphiconType + ' white"></span> <span class="white">'+ btnText +'</span>'
   end
 end
